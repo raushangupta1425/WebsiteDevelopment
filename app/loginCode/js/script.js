5ea8399136cc29eb93f4fc5faf46f7c742348f86
Raushan Gupta <126952814+raushangupta1425@users.noreply.github.com>
2024-01-08 07:39:09 +0000

    c

--- app/loginCode/js/script.js
+++ app/loginCode/js/script.js
@@ -139,7 +139,7 @@ login_frm.onsubmit = function (){
     if(localStorage.getItem(loginId) != null){
         if(loginPassword == obj_data.password){
             sessionStorage.setItem("userID",loginId);
-            window.location.replace("./app/home_page/homepage.html");
+            window.location.replace("../../../app/home_page/homepage.html");
         }else{
             let wrong_pass = document.getElementById("wrong_pass");
             wrong_pass.innerHTML = "<i class='fa fa-exclamation-triangle' aria-hidden='true'></i> Wrong Password!";
