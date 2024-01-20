2feb3e60d5edd8db01b6dc109677a044d21926c2
Raushan Gupta <126952814+raushangupta1425@users.noreply.github.com>
2024-01-20 08:13:03 +0000

    player update

--- app/home_page/sub_pages/player/js/script.js
+++ app/home_page/sub_pages/player/js/script.js
@@ -1,15 +1,19 @@
 // Player scripting here.
 let video_src = document.getElementById("video_src");
+let dis_name = document.getElementById("dis_name");
 let link = sessionStorage.getItem("link");
+let yt_btn_link = sessionStorage.getItem("youtube");
+let v_name = sessionStorage.getItem("name");
 let youtube_link_btn = document.getElementById("youtube_link");
 let description_btn = document.getElementById("description");
 let arrow = document.getElementById("down_arrow");
 
 video_src.src = link;
+dis_name.innerHTML = v_name;
 // video_src.setAttribute("src",link); 
 
 youtube_link_btn.onclick = function (){
-    document.location.href = link;
+    document.location.href = yt_btn_link;
 }
 
 $(document).ready(function (){
